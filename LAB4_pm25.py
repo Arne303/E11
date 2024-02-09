fdca4057c2f435fdadd0cc9d1c440044d2856e48
Arne303 <49143169+Arne303@users.noreply.github.com>
2024-02-09 15:15:46 -0800

    Update LAB4_pm25.py

--- LAB4_pm25.py
+++ LAB4_pm25.py
@@ -7,6 +7,7 @@ Example sketch to connect to PM2.5 sensor with either I2C or UART.
 
 # pylint: disable=unused-import
 import time
+import csv
 import board
 import busio
 from digitalio import DigitalInOut, Direction, Pull
@@ -14,40 +15,26 @@ from adafruit_pm25.i2c import PM25_I2C
 
 
 reset_pin = None
-# If you have a GPIO, its not a bad idea to connect it to the RESET pin
-# reset_pin = DigitalInOut(board.G0)
-# reset_pin.direction = Direction.OUTPUT
-# reset_pin.value = False
-
-
-# For use with a computer running Windows:
-# import serial
-# uart = serial.Serial("COM30", baudrate=9600, timeout=1)
-
-# For use with microcontroller board:
-# (Connect the sensor TX pin to the board/computer RX pin)
-# uart = busio.UART(board.TX, board.RX, baudrate=9600)
 
 # For use with Raspberry Pi/Linux:
 import serial
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
 
-# For use with USB-to-serial cable:
-# import serial
-# uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=0.25)
-
 # Connect to a PM2.5 sensor over UART
 from adafruit_pm25.uart import PM25_UART
 pm25 = PM25_UART(uart, reset_pin)
 
-# Create library object, use 'slow' 100KHz frequency!
-# i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
-# Connect to a PM2.5 sensor over I2C
-# pm25 = PM25_I2C(i2c, reset_pin)
-
 print("Found PM2.5 sensor, reading data...")
 
-while True:
+meta_data = ["Time", "PM2.5", "PM10"]
+
+file = open("aq_data.csv","w",newline='')
+
+writer = csv.writer(file)
+writer.writerow(meta_data)
+
+i = 0
+while i<30:
     time.sleep(1)
 
     try:
@@ -64,6 +51,14 @@ while True:
         "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
         % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
     )
+
+    now = time.time()
+    data_out = [now,aqdata["pm25 standard"], aqdata["pm100 standard"]]
+    writer.writerow(data_out)
+
+    i = i + 1
+
+    """
     print("Concentration Units (environmental)")
     print("---------------------------------------")
     print(
@@ -78,3 +73,6 @@ while True:
     print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
     print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
     print("---------------------------------------")
+    """
+
+file.close()
