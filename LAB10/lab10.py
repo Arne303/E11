a854e7e70896c0226617a341237027c02b8338bd
Arne303 <49143169+Arne303@users.noreply.github.com>
2024-03-22 14:46:12 -0700

    Create lab10.py

---
+++ LAB10/lab10.py
@@ -0,0 +1,48 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+import csv
+
+channel = 16
+count = 0
+
+def my_callback(channel):
+    print(time.time())
+    global count
+    count = count + 1
+    print(count)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(channel, GPIO.FALLING, callback=my_callback)
+
+print(sys.argv) # arguments that were given when starting "python daq_test_week5.py arguments"; first [0] is the program name
+
+
+run_time = 60
+run_time = int(sys.argv[1]) # using first input argument
+
+print("Found sensors, reading data...")
+
+filename = "data.csv"
+filename = sys.argv[3]
+file = open(filename,"w",newline='')
+writer = csv.writer(file)
+
+meta_data = ["Time","Counts"]
+writer.writerow(meta_data)
+
+start_time = time.time()
+
+now = time.time()
+while (now-start_time) < run_time:
+    count = 0
+    time.sleep(sys.argv[2])
+
+    now = time.time()
+    print(now, "-", "Collected", count, "counts.")
+
+    writer.writerow(now, count)
+
+file.close()
+print("Data collection complete!")
