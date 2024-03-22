1ceca0dd373e0381f91f5863046d14922a746c94
Arne303 <49143169+Arne303@users.noreply.github.com>
2024-03-22 14:56:34 -0700

    Update lab10.py

--- LAB10/lab10.py
+++ LAB10/lab10.py
@@ -11,6 +11,7 @@ def my_callback(channel):
     global count
     count = count + 1
     print(count)
+    print( )
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -18,16 +19,18 @@ GPIO.add_event_detect(channel, GPIO.FALLING, callback=my_callback)
 
 print(sys.argv) # arguments that were given when starting "python daq_test_week5.py arguments"; first [0] is the program name
 
-
 run_time = 60
 run_time = int(sys.argv[1]) # using first input argument
 
-print("Found sensors, reading data...")
+looptime = 10
+looptime = int(sys.argv[2])
 
 filename = "data.csv"
 filename = sys.argv[3]
 file = open(filename,"w",newline='')
 writer = csv.writer(file)
+
+print("--- Measurement starting ---")
 
 meta_data = ["Time","Counts"]
 writer.writerow(meta_data)
@@ -37,9 +40,6 @@ start_time = time.time()
 now = time.time()
 while (now-start_time) < run_time:
     count = 0
-
-    looptime = 10
-    looptime = int(sys.argv[2])
     time.sleep(looptime)
 
     now = time.time()
